2a717b338fdbcf3df9646225b92ce944cbadff98
Siebenundzwanzig <spiele-registrierer@web.de>
2019-08-21 15:03:20 +0200

    midway equalizaion with numpy, also imgs as input

--- python/midway_equal_imgs.py
+++ python/midway_equal_imgs.py
@@ -8,33 +8,33 @@ import cv2 as cv
 import video
 import matplotlib.pyplot as plt
 
-nr_frames = 1
-path = '/home/laars/uni/BA/eyes/Gold_Standard_Kopien/1/1d38e5a6-7b4b-49cd-b23c-8dc92b35c265/1d38e5a6-7b4b-49cd-b23c-8dc92b35c265/000/eye1.mp4'#'/home/laars/uni/BA/eyes/000/eye1.mp4'
+nr_frames = 2
+path = '/home/laars/uni/BA/code/python/results/wflicker/'#'/home/laars/uni/BA/eyes/000/eye1.mp4'
 
 
-def midway_equal(   path = path,
+def midway_equal_imgs(   path = path,
                     nr_frames = nr_frames,
                     save_path='/home/laars/uni/BA/code/python/results/deflicker/'
                 ):
-    # frames = []
-
-    cap = video.create_capture(path)
-    width = int(cap.get(3))
-    height = int(cap.get(4))
-    sum_sort = np.zeros((width * height))
-    indices = []#np.zeros((nr_frames, width*height),dtype=np.uint16)
-    frames_def = np.zeros((nr_frames, height, width))
+    imgs = []
+    for z in range(nr_frames):
+        path_file = path + 'flicker%s.png' % (z)
+        imgs.append(cv.imread(path_file, 0))
+    width = imgs[0].shape[1]
+    height = imgs[0].shape[0]
+    sum_sort = np.zeros((width * height),dtype=np.uint16)
+    indices = np.zeros((nr_frames, width*height),dtype=np.uint32)
+    frames_def = np.zeros((nr_frames, height, width), dtype=np.uint8)
 
     for x in range(nr_frames):
-        _ret, frame = cap.read()
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        # cv.imshow('frame', frame)
-        # cv.waitKey(0)
+
+        cv.imshow('frame', imgs[x])
+        cv.waitKey(0)
 
         # print(np.unique(frame, return_counts=True))
         # frames.append(frame)
-        flat = np.concatenate(frame)
-        indices.append(flat.argsort().argsort())
+        flat = np.concatenate(imgs[x])
+        indices[x] = flat.argsort().argsort()
         # print(np.unique(np.sort(flat), return_counts=True))
         sort = np.sort(flat)
         # hist = cv.calcHist(frame, [0], None,[256],[0,256])
@@ -43,25 +43,27 @@ def midway_equal(   path = path,
         sum_sort = sum_sort + sort
     mean = np.array(sum_sort/nr_frames, dtype=np.uint8)
 
-    # print(np.unique(mean, return_counts=True))
 
     for y in range(nr_frames):
+        # print(mean.shape)
+        # print(indices)
+
         backsort = mean[indices[y]]
-        print(indices)
         frames_def[y] = np.reshape(backsort, (height, width))
+        # print(np.unique(frames_def, return_counts=True))
         # hist = cv.calcHist(frame_def, [0], None,[256],[0,256])
         # plt.plot(hist)
         # plt.show()
 
 
-        # cv.imshow('frame_def', frame_def)
-        # cv.waitKey(0)
+        cv.imshow('frame_def', frames_def[y])
+        cv.waitKey(0)
         if save_path is not None:
-            cv.imwrite('%s%s.png' % (save_path, y+1),
+            cv.imwrite('%s%s.png' % (save_path, y),
                         frames_def[y]
                         )
 
     return frames_def
 
 if __name__== '__main__':
-    midway_equal()
+    midway_equal_imgs()
